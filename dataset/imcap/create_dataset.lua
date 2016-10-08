891bcba58c5f36d7db24e2409b0e65334bb728e7
Xiao Lin <linxiao.frank@gmail.com>
2016-10-08 04:38:08 -0400

    refreshing imcap dataset creation

--- dataset/imcap/create_dataset.lua
+++ dataset/imcap/create_dataset.lua
@@ -1,4 +1,26 @@
+--Join images with captions to make train val test datasets
+cmd = torch.CmdLine();
+cmd:text('Combine images with captions to create image-captioning datasets: 82783/5000/5000');
+cmd:text('Options')
+cmd:option('-image','../mscoco/mscoco_vgg19_center.t7','t7 of image features');
+cmd:option('-caption','../mscoco/captions.json','JSON of tokenized captions');
+params=cmd:parse(arg);
+--print(params)
 
+cjson=require('cjson');
+function readAll(file)
+    local f = io.open(file, "r")
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+function loadJson(fname)
+	local t=readAll(fname)
+	return cjson.decode(t)
+end
+
+
+--figure out splits
 function readlines(fname)
 	local data={};
 	local f=torch.DiskFile(fname,'r');
@@ -20,98 +42,52 @@ function reverse_table(tbl)
 	end
 	return t;
 end
+function index(data,dictionary)
+	local ind=torch.LongTensor(#data);
+	for i=1,#data do
+		ind[i]=dictionary[data[i]];
+	end
+	return ind;
+end
+function slice_cell(data,ind)
+	local new_data={};
+	for i=1,ind:size(1) do
+		table.insert(new_data,data[ind[i]]);
+	end
+	return new_data;
+end
+
+--Load json files
+images=torch.load(params.image);
+images.lookup=reverse_table(images.imname);
+captions=loadJson(params.caption);
+captions.lookup=reverse_table(captions.imname);
 
 
-N=5;
 train_ims=readlines('./splits/coco_train.txt');
 for i=1,#train_ims do
 	train_ims[i]='train2014/'..train_ims[i];
 end
-
 val_ims=readlines('./splits/coco_val.txt');
 for i=1,#val_ims do
 	val_ims[i]='val2014/'..val_ims[i];
 end
-
 test_ims=readlines('./splits/coco_test.txt');
 for i=1,#test_ims do
 	test_ims[i]='val2014/'..test_ims[i];
 end
 
-ntrain=#train_ims;
-nval=#val_ims;
-ntest=#test_ims;
-print(string.format('Train:%d',ntrain));
-print(string.format('Val:%d',nval));
-print(string.format('Test:%d',ntest));
-
-caps=torch.load('../mscoco/mscoco_captions.t7');
-ims=torch.load('../mscoco/mscoco_vgg_center_v2.t7')
+--Save t7s
+train_ind=index(train_ims,images.lookup);
+train_ind_caption=index(train_ims,captions.lookup);
+torch.save('dataset_train.t7',{image={fvs=images.fvs:index(1,train_ind):clone(),imname=train_ims,lookup=reverse_table(train_ims)},caption={caption=slice_cell(captions.caption,train_ind_caption),imname=train_ims,lookup=reverse_table(train_ims)}});
 
 
-caps['ims_id']=reverse_table(caps['ims']);
-ind5_train=torch.LongTensor(ntrain*N);
-for i=1,ntrain do
-	for j=1,N do
-		ind5_train[N*(i-1)+j]=N*(caps['ims_id'][train_ims[i]]-1)+j;
-	end
-end
-
-ind5_val=torch.LongTensor(nval*N);
-for i=1,nval do
-	for j=1,N do
-		ind5_val[N*(i-1)+j]=N*(caps['ims_id'][val_ims[i]]-1)+j;
-	end
-end
-
-ind5_test=torch.LongTensor(ntest*N);
-for i=1,ntest do
-	for j=1,N do
-		ind5_test[N*(i-1)+j]=N*(caps['ims_id'][test_ims[i]]-1)+j;
-	end
-end
-
-ims['ims_id']=reverse_table(ims['ims']);
-ind_train=torch.LongTensor(ntrain);
-for i=1,ntrain do
-	ind_train[i]=ims['ims_id'][train_ims[i]];
-end
-
-ind_val=torch.LongTensor(nval);
-for i=1,nval do
-	ind_val[i]=ims['ims_id'][val_ims[i]];
-end
-
-ind_test=torch.LongTensor(ntest);
-for i=1,ntest do
-	ind_test[i]=ims['ims_id'][test_ims[i]];
-end
-
-data_train={};
-data_train['ims']=train_ims;
-data_train['caption_tokens']=caps['tokens']:index(1,ind5_train);
-data_train['image_fvs']=ims['fvs']:index(1,ind_train);
-data_train['caption_dictionary']=caps['dictionary'];
-data_train['ncaptions_per_im']=N;
+val_ind=index(val_ims,images.lookup);
+val_ind_caption=index(val_ims,captions.lookup);
+torch.save('dataset_val.t7',{image={fvs=images.fvs:index(1,val_ind):clone(),imname=val_ims,lookup=reverse_table(val_ims)},caption={caption=slice_cell(captions.caption,val_ind_caption),imname=val_ims,lookup=reverse_table(val_ims)}});
 
 
-data_val={};
-data_val['ims']=val_ims;
-data_val['caption_tokens']=caps['tokens']:index(1,ind5_val);
-data_val['image_fvs']=ims['fvs']:index(1,ind_val);
-data_val['caption_dictionary']=caps['dictionary'];
-data_val['ncaptions_per_im']=N;
-
-
-data_test={};
-data_test['ims']=test_ims;
-data_test['caption_tokens']=caps['tokens']:index(1,ind5_test);
-data_test['image_fvs']=ims['fvs']:index(1,ind_test);
-data_test['caption_dictionary']=caps['dictionary'];
-data_test['ncaptions_per_im']=N;
-
-
-
-torch.save('dataset_train.t7',data_train);
-torch.save('dataset_val.t7',data_val);
-torch.save('dataset_test.t7',data_test);
+test_ind=index(test_ims,images.lookup);
+test_ind_caption=index(test_ims,captions.lookup);
+torch.save('dataset_test.t7',{image={fvs=images.fvs:index(1,test_ind):clone(),imname=test_ims,lookup=reverse_table(test_ims)},caption={caption=slice_cell(captions.caption,test_ind_caption),imname=test_ims,lookup=reverse_table(test_ims)}});
